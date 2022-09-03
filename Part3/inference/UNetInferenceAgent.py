4b1477369556521a5f731f6483e35dd213aa91b1
Miguel Ángel <mikweeh@gmail.com>
2022-09-03 10:06:25 +0000

    Add files via upload

---
+++ Part3/inference/UNetInferenceAgent.py
@@ -0,0 +1,77 @@
+"""
+Contains class that runs inferencing
+"""
+import torch
+import numpy as np
+
+from networks.RecursiveUNet import UNet
+
+from utils.utils import med_reshape
+
+class UNetInferenceAgent:
+    """
+    Stores model and parameters and some methods to handle inferencing
+    """
+    def __init__(self, parameter_file_path='', model=None, device="cpu", patch_size=64):
+
+        self.model = model
+        self.patch_size = patch_size
+        self.device = device
+
+        if model is None:
+            self.model = UNet(num_classes=3)
+
+        if parameter_file_path:
+            self.model.load_state_dict(torch.load(parameter_file_path, map_location=self.device))
+
+        self.model.to(device)
+
+    def single_volume_inference_unpadded(self, volume):
+        """
+        Runs inference on a single volume of arbitrary patch size,
+        padding it to the conformant size first
+
+        Arguments:
+            volume {Numpy array} -- 3D array representing the volume
+
+        Returns:
+            3D NumPy array with prediction mask
+        """
+
+        # First reshape
+        volume = med_reshape(volume, (volume.shape[0], self.patch_size, self.patch_size))
+
+        # And then return a single volume inference
+        return self.single_volume_inference(volume)
+
+    def single_volume_inference(self, volume):
+        """
+        Runs inference on a single volume of conformant patch size
+
+        Arguments:
+            volume {Numpy array} -- 3D array representing the volume
+
+        Returns:
+            3D NumPy array with prediction mask
+        """
+        self.model.eval()
+
+        # Assuming volume is a numpy array of shape [X,Y,Z] and we need to slice X axis
+        slices = []
+
+        # TASK: Write code that will create mask for each slice across the X (0th) dimension. After 
+        # that, put all slices into a 3D Numpy array. You can verify if your method is 
+        # correct by running it on one of the volumes in your training set and comparing 
+        # with the label in 3D Slicer.
+        # <YOUR CODE HERE>
+
+        # Built the input tensor in the correct format (X, 1, Y, Z)
+        input = torch.from_numpy(volume).float().to(self.device).unsqueeze(1)
+
+        # Forward pass to get the output of the network
+        output = self.model(input)
+
+        # Get max indices and convert them to numpy array
+        slices = torch.argmax(output.cpu().detach(), 1).numpy()
+
+        return slices
